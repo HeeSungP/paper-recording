b190d6291ad5718cef7ddf3d526214cd69a60990
HeesungPark <gregmark@naver.com>
2021-09-23 10:05:00 +0900

    [20210923] 실험 업로드

---
+++ hs_data_v4.py
@@ -0,0 +1,156 @@
+# -*- coding:utf-8 -*-
+
+from keras.preprocessing.image import img_to_array, load_img
+import numpy as np
+import glob
+
+
+class dataProcess(object):
+    def __init__(self, out_rows, out_cols, train_path="PPtest/train", train_label="PPtest/trainannot",
+                 guide_path = 'PPtest/guide', custom_name = "/hs_v4", # / 앞에 붙어야하고 뒤에 _ 붙음
+                 test_path="PPtest/test", test_label='PPtest/testannot', npy_path="./npydata", img_type="jpg"):
+        self.out_rows = out_rows
+        self.out_cols = out_cols
+        self.train_path = train_path
+        self.train_label = train_label
+        self.img_type = img_type
+        #self.val_path = val_path
+        #self.val_label = val_label
+        self.test_path = test_path
+        self.test_label = test_label
+        self.npy_path = npy_path
+        self.custom_name = custom_name
+        self.guide_path = guide_path
+
+#     def label2class(self, label):
+#         x = np.zeros([self.out_rows, self.out_cols, 12])
+#         for i in range(self.out_rows):
+#             for j in range(self.out_cols):
+#                 x[i, j, int(label[i][j])] = 1  # 3차원 m 지점에서는 1번째 m 클래스에 속한다.
+#         return x
+    def label2class(self, label):
+        x = np.zeros([self.out_rows, self.out_cols])
+        black = np.array((0,0,0)) # 외벽 1 / 엣지
+        #blue = np.array((0,0,255)) # 창문 2
+        white = np.array((255,255,255)) # 배경 3 / 나머지
+
+        for i in range(self.out_rows):
+            for j in range(self.out_cols):
+                #x[i, j, int(label[i][j])] = 1  # 3차원 m 지점에서는 1번째 m 클래스에 속한다.
+                temp = (np.linalg.norm(label[i][j]-white), np.linalg.norm(label[i][j]-black))
+                temp_idx = temp.index(min(temp))
+                x[i, j] = temp_idx # 0화이트 배경 1블랙 엣지
+
+        return x
+
+
+
+    def create_train_data(self):
+        i = 0
+        print('Creating training images...')
+        imgs0 = sorted(glob.glob(self.train_path+"/*."+self.img_type))
+        imgs = imgs0 #+ imgs1
+        imgs = [x.replace('\\','/') for x in imgs] #hs
+
+        labels0 = sorted(glob.glob(self.train_label+"/*."+self.img_type))
+        labels = labels0 #+ labels1
+        labels = [x.replace('\\','/') for x in labels] #hs
+
+        imgdatas = np.ndarray((len(imgs), self.out_rows, self.out_cols,2), dtype=np.uint8)
+        guidedatas = np.ndarray((len(imgs), self.out_rows, self.out_cols, 1), dtype=np.uint8)
+        imglabels = np.ndarray((len(labels), self.out_rows, self.out_cols,1), dtype=np.uint8)
+        print(len(imgs), len(labels))
+
+        for x in range(len(imgs)):
+            imgpath = imgs[x]
+            labelpath = labels[x]
+
+            img = load_img(imgpath, grayscale=True, target_size=[512, 512])
+            label = load_img(labelpath, grayscale=True, target_size=[512, 512])
+            guide = load_img(self.guide_path + '/' + imgs[i].split('/')[-1], grayscale=True, target_size=[512, 512])
+
+            img = img_to_array(img)
+            label = img_to_array(label)
+            guide = img_to_array(guide)
+
+            imgdatas[i] = np.concatenate((img, guide), axis=2) # imgdatas[i] = img
+            imglabels[i] = label
+            guidedatas[i] = guide
+
+            if i % 60 == 0:
+                print('Done: {0}/{1} images'.format(i, len(imgs)))
+            i += 1
+
+        print('loading done')
+        np.save(self.npy_path + self.custom_name + '_train.npy', imgdatas)
+        np.save(self.npy_path + self.custom_name + '_guide_train.npy', guidedatas)
+        np.save(self.npy_path + self.custom_name + '_mask_train.npy', imglabels)
+        print('Saving to .npy files done.')
+
+    def create_test_data(self):
+        i = 0
+        print('Creating test images...')
+        imgs = glob.glob(self.test_path + "/*." + self.img_type) #hs
+        imgs = [x.replace('\\','/') for x in imgs]
+
+        imgdatas = np.ndarray((len(imgs), self.out_rows, self.out_cols,2), dtype=np.uint8)
+        guidedatas = np.ndarray((len(imgs), self.out_rows, self.out_cols, 1), dtype=np.uint8)
+        testpathlist = []
+
+        for imgname in imgs:
+            testpath = imgname
+            testpathlist.append(testpath)
+            img = load_img(testpath, grayscale=True, target_size=[512, 512])
+            guide = load_img(self.guide_path + '/' + imgs[i].split('/')[-1], grayscale=True, target_size=[512, 512])
+
+            img = img_to_array(img)
+            guide = img_to_array(guide)
+
+            imgdatas[i] = np.concatenate((img, guide), axis=2) # imgdatas[i] = img
+            guidedatas[i] = guide
+            i += 1
+
+        txtname = './results' + self.custom_name + '.txt'
+        with open(txtname, 'w') as f:
+            for i in range(len(testpathlist)):
+                f.writelines(testpathlist[i] + '\n')
+        print('loading done')
+        np.save(self.npy_path + self.custom_name +'_test.npy', imgdatas)
+        np.save(self.npy_path + self.custom_name + '_guide_test.npy', guidedatas)
+        print('Saving to imgs_test.npy files done.')
+
+    def load_train_data(self):
+        print('load train images...')
+        imgs_train = np.load(self.npy_path + self.custom_name + "_train.npy")
+        imgs_guide_train = np.load(self.npy_path + self.custom_name + "_guide_train.npy")
+        imgs_mask_train = np.load(self.npy_path + self.custom_name + "_mask_train.npy")
+
+        imgs_train = imgs_train.astype('float32')
+        imgs_guide_train = imgs_guide_train.astype('float32')
+        imgs_mask_train = imgs_mask_train.astype('float32')
+        imgs_train /= 255
+        imgs_guide_train /= 255
+        imgs_mask_train /= 255
+        return imgs_train, imgs_guide_train, imgs_mask_train
+
+    def load_test_data(self):
+        print('-' * 30)
+        print('load test images...')
+        print('-' * 30)
+        imgs_test = np.load(self.npy_path + self.custom_name + "_test.npy")
+        imgs_guide_test = np.load(self.npy_path + self.custom_name + "_guide_test.npy")
+
+        imgs_test = imgs_test.astype('float32')
+        imgs_guide_test = imgs_guide_test.astype('float32')
+
+        imgs_test /= 255
+        imgs_guide_test /= 255
+
+        return imgs_test, imgs_guide_test
+
+
+
+if __name__ == "__main__":
+    mydata = dataProcess(512, 512)
+    mydata.create_train_data()
+    mydata.create_test_data()
---
+++ hs_model_v0_e.py
@@ -0,0 +1,151 @@
+# -*- coding:utf-8 -*-
+
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint
+from keras import metrics
+from keras import initializers
+import cv2
+from hs_data_v0_e import *
+
+# np.random.seed(777) # for reproducibility
+# initializers.he_normal(seed=777)
+
+
+class myUnet(object):
+    def __init__(self, img_rows=512, img_cols=512, custom_name='/hs_v0_e'):
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+        self.custom_name = custom_name
+
+    def load_data(self):
+        mydata = dataProcess(self.img_rows, self.img_cols)
+        imgs_train, imgs_guide_train, imgs_mask_train = mydata.load_train_data()
+        imgs_test, imgs_guide_test = mydata.load_test_data()
+        return imgs_train, imgs_guide_train, imgs_mask_train, imgs_test, imgs_guide_test
+
+    def get_unet(self):
+        inputs = Input((self.img_rows, self.img_cols, 1))
+        guides = Input((self.img_rows, self.img_cols, 1))
+        print(inputs.shape)
+        print(guides.shape)
+
+        conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(guides)
+        conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+        conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+        conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+        conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+        conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+        conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+        conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+        drop4 = Dropout(0.5)(conv4)
+        pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+        conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+        conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+        drop5 = Dropout(0.5)(conv5)
+
+        up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            UpSampling2D(size=(2, 2))(drop5))
+        concatenate6 = concatenate([drop4, up6], axis=3)
+        conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(concatenate6)
+        conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+        up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            UpSampling2D(size=(2, 2))(conv6))
+        concatenate7 = concatenate([conv3, up7], axis=3)
+        conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(concatenate7)
+        conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+        up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            UpSampling2D(size=(2, 2))(conv7))
+        concatenate8 = concatenate([conv2, up8], axis=3)
+        conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(concatenate8)
+        conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+
+        up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            UpSampling2D(size=(2, 2))(conv8))
+        concatenate9 = concatenate([conv1, up9], axis=3)
+        conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(concatenate9)
+        conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+        conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9) # 3 2
+
+        # v1
+        # concatenate10 = concatenate([conv9, guides], axis=3)
+        # conv10 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(concatenate10)
+        # conv10 = Conv2D(1, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv10)
+        # conv10 = Conv2D(1, 1, activation='sigmoid')(conv10)
+
+        conv10 = Conv2D(1, 1, activation='sigmoid')(conv9) #hs 1로수정 softmax sigmoid
+        
+        model = Model([inputs, guides], conv10)
+
+        model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy']) # categorical_crossentropy binary_crossentropy
+
+        return model
+
+    def train(self):
+        print("loading data")
+        imgs_train, imgs_guide_train, imgs_mask_train, imgs_test, imgs_guide_test = self.load_data()
+        print("loading data done")
+        model = self.get_unet()
+        print("got unet")
+        model_checkpoint = ModelCheckpoint('./weights' + self.custom_name + '_unet.hdf5', monitor='loss', verbose=1, save_best_only=True)
+        print('Fitting model...')
+        model.fit((imgs_train, imgs_guide_train), imgs_mask_train, batch_size=4, epochs=50, verbose=1,
+                  validation_split=0.1, shuffle=True, callbacks=[model_checkpoint])
+
+        print('predict test data')
+        print('test candidate data : {}'.format(imgs_test.shape))
+        imgs_mask_test = model.predict((imgs_test, imgs_guide_test), batch_size=1, verbose=1)
+        print('test result data : {}'.format(imgs_mask_test.shape))
+        np.save('./results' + self.custom_name + '_mask_test.npy', imgs_mask_test)
+
+    def save_img(self):
+        print("array to image")
+        imgs = np.load('./results' + self.custom_name + '_mask_test.npy')
+        piclist = []
+        for line in open("./results" + self.custom_name + ".txt"):
+            line = line.strip()
+            picname = line.split('/')[-1]
+            piclist.append(picname)
+        for i in range(imgs.shape[0]):
+            path = "./results/" + piclist[i]
+            img = np.zeros((imgs.shape[1], imgs.shape[2], 3), dtype=np.uint8)
+            for k in range(len(img)):
+                for j in range(len(img[k])):
+                    # 0화이트 배경 1블랙 엣지
+                    if float(imgs[i][k][j]) >= 0.5:
+                        img[k][j]=[255,255,255]
+                    elif float(imgs[i][k][j]) < 0.5:
+                        img[k][j]=[0,0,0]
+
+
+                    # num = np.argmax(imgs[i][k][j])
+                    # if num == 0:
+                    #     #img[k][j] = [128, 128, 128]
+                    #     img[k][j] = [0, 0, 0]
+                    # elif num == 1:
+                    #     #img[k][j] = [128, 0, 0]
+                    #     #img[k][j] = [255, 0, 0]
+                    #     img[k][j] = [255, 255, 255]
+                    # elif num == 2:
+                    #     #img[k][j] = [192, 192, 128]
+                    #     img[k][j] = [255, 255, 255]
+
+            img = cv2.resize(img, (512, 512), interpolation=cv2.INTER_CUBIC)
+            cv2.imwrite(path, img)
+
+
+if __name__ == '__main__':
+    myunet = myUnet()
+    model = myunet.get_unet()
+    model.summary()
+    # plot_model(model, to_file='model.png')
+    myunet.train()
+    myunet.save_img()
